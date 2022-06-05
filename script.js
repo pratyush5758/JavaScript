177a2efdcf33613bc697e7df434592900879e725
Pratyush Kumar <pratyushkumar@Pratyushs-MacBook-Air.local>
2022-06-05 12:56:41 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,54 @@
+// const now = 2022;
+// const agepratyush = now - 2000;
+// const ageprayasha = now - 2002;
+// console.log(now - 2000 > now - 2002);
+
+// console.log(25 - 10 - 5);
+
+// let x, y;
+// x = y = 25 - 10 - 5; // x = y = 10//
+// console.log(x, y);
+// const averageAge = (agepratyush + ageprayasha) / 2
+// console.log(agepratyush, ageprayasha, averageAge);
+
+//#coding challenge #1//
+
+// const massmark = 78;
+// const heightmark = 1.69;
+// const massjohn = 92;
+// const heightjohn = 1.95;
+
+// const bmimark = massmark / heightmark ** 2;
+// const bmijohn = massjohn / (heightjohn * heightjohn);
+// const markhigherbmi = bmimark > bmijohn;
+// console.log(bmimark, bmijohn, markhigherbmi);
+
+// const firstname = "pratyush";
+// const job = "student";
+// const birthyear = 2000;
+// const year = 2022;
+
+// const pratyush = "I'm " + firstname + ', a ' + (year - birthyear) + ' years old ' +  job  + '!';
+// console.log(pratyush);
+
+// const pratyushnew = `I'm ${firstname}, a ${year - birthyear} year old ${job}!`;
+// console.log(pratyushnew);
+
+// console.log('hello worlds');
+
+
+// div - 5 print - A
+// div - 10 print - B
+// div - Both print - C
+
+function div(number) {
+    if(number % 5 === 0){
+        console.log("A");
+    }else if(number % 10 === 0){
+        console.log("B");
+    }else{
+        console.log("C");
+    }
+    
+}
+console.log(div(20));
